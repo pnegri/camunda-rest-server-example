5f35fea55cf3974b5322a9205891392be9e89d86
Patrick Negri <patrick@iugu.com>
2016-07-26 09:49:31 -0300

    [ ! ] Initial Commit

---
+++ src/main/java/iugubpmn/rest/RestProcessEngineDeployment.java
@@ -0,0 +1,22 @@
+package iugubpmn.rest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+import org.camunda.bpm.engine.rest.impl.CamundaRestResources;
+
+public class RestProcessEngineDeployment extends Application {
+
+  @Override
+  public Set<Class<?>> getClasses() {
+    Set<Class<?>> classes = new HashSet<Class<?>>();
+
+    classes.addAll(CamundaRestResources.getResourceClasses());
+    classes.addAll(CamundaRestResources.getConfigurationClasses());
+
+    return classes;
+  }
+
+}
